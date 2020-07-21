ae6aae83fd717bc4af8fde1055ff997cd20ad850
Josiahmpokera <jmpokera@outlook.com>
2020-07-21 21:42:58 +0300

    Javascript file modified

--- function.js
+++ function.js
@@ -6,8 +6,6 @@ var projectTitle = document.getElementById('title');
 var submitButton = document.getElementById('submit').addEventListener('click', ShowIt);
 
 function ShowIt(){
-    console.log("User input ii not categorized");
-    console.log(email.value.toString());
     console.log(name.value);
     console.log(projectTitle.value);
 }
